7498cedd9f115fc612f882b1ef9017dba201cbab
manshay_verma <manshayverma4@gmail.com>
2026-05-16 15:32:33 +0530

    hyper-link connection stablish for portfolio

--- profile_1/urls.py
+++ profile_1/urls.py
@@ -3,4 +3,6 @@ from django.urls import path,  include
 import profile_1.views
 urlpatterns = [
     path("", profile_1.views.home, name="home"),
+    path("portfolio-single/<str:name>/", profile_1.views.portfolio_single, name="portfolio_single"),
 ]
+
--- profile_1/views.py
+++ profile_1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'portfolio/base.html')
+
+def portfolio_single(request, name):
+    return render(request, f'portfolio/{name}.html')
